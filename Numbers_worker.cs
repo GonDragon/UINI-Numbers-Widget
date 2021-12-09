93df3aed9edd15ce3d00c1919a99be268d72a99f
Gonzalo Romero <gondragon13@gmail.com>
2021-12-09 14:23:48 -0300

    Removed tooltip

--- Numbers_worker.cs
+++ Numbers_worker.cs
@@ -14,6 +14,7 @@ namespace UINotIncluded.Widget.Workers
     {
         public Numbers_worker(Configs.NumbersConfig config) : base(config) { }
 
+        public override bool ShowTooltip => false;
         public override void OpenConfigWindow()
         {
             Find.WindowStack.Add(new UINotIncluded.Windows.EditMainButton_Window(this.config));
